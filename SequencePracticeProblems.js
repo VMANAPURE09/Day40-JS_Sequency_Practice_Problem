dabf8457f2ee4f5cdfe08ad729e9e7a7f71ea655
tutorial6 <manapure.vaibhav542@gmail.com>
2022-07-31 21:25:16 +0530

    UC1:Random Function Math.floor(Math.random() * 10); to get Single Digit

---
+++ SequencePracticeProblems.js
@@ -0,0 +1,5 @@
+console.log("Welcome to Sequence Practice Problems")
+
+//UC1:Random Function Math.floor(Math.random() * 10); to get Single Digit.
+let singleDigitRandomNumber = Math.floor(Math.random() * 10);  
+console.log("Random single digit : "+singleDigitRandomNumber);
